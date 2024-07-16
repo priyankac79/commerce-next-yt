8a2974d61484922fbadb0900547bd9224b619ba8
priyanka <priyankachikhale7@gmail.com>
2024-07-16 13:54:29 +0530

    finished

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import AddToBag from "@/app/components/AddToBag";
+import CheckoutNow from "@/app/components/CheckoutNow";
 import ImageGallery from "@/app/components/ImageGallery";
 import { fullProduct } from "@/app/interface";
 import { client } from "@/app/lib/sanity";
@@ -21,6 +22,8 @@ async function getData(slug: string) {
 
 	return data;
 }
+
+export const dynamic = "force-dynamic";
 
 export default async function productPage({
 	params,
@@ -86,7 +89,15 @@ export default async function productPage({
 								key={data._id}
 								price_id={data.price_id}
 							/>
-							<Button variant={"secondary"}>Checkout Now</Button>
+							<CheckoutNow
+								currency="USD"
+								description={data.description}
+								image={data.images[0]}
+								name={data.name}
+								price={data.price}
+								key={data._id}
+								price_id={data.price_id}
+							/>
 						</div>
 
 						<p className="mt-12 text-base text-gray-500">{data.description}</p>
