c4f6d21f0a9bd915da51c1c8e016cc8defe3be41
priyanka <priyankachikhale7@gmail.com>
2024-07-16 17:40:15 +0530

    sanity deployed

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="text-gray-600 text-center pb-2">
+        <footer className="text-gray-600 text-center py-2">
             @2024. All rights reserved
         </footer>
     )
